e8b1ac3486b446819e0d1d310c4af85fef99c49e
wijeremy <jeremydavwilliams@gmail.com>
2021-10-15 14:02:55 -0500

    added something cool

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TypeIt from 'typeit-react';
 
 const Nav = () => {
   const styles = {
@@ -7,51 +8,53 @@ const Nav = () => {
     },
   };
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark code">
-      <div className="container-fluid code">
-        <a className="navbar-brand code-link" href="/">
-          Jeremy Williams
-        </a>
-        <button
-          className="navbar-toggler code"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse code" id="navbarNav">
-          <ul className="navbar-nav code">
-            <li className="nav-item code" key="About">
-              <a
-                className="nav-link code-link"
-                href="#about"
-                style={styles.bright}
-              >
-                About
-              </a>
-            </li>
-            <li className="nav-item code-link" key="Code">
-              <a className="nav-link code" href="#code" style={styles.bright}>
-                Code
-              </a>
-            </li>
-            <li className="nav-item code-link" key="Information">
-              <a
-                className="nav-link code"
-                href="#information"
-                style={styles.bright}
-              >
-                Information
-              </a>
-            </li>
-          </ul>
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark code">
+        <div className="container-fluid code">
+          <a className="navbar-brand code-link" href="/" id="me">
+            <TypeIt>Jeremy Williams</TypeIt>
+          </a>
+          <button
+            className="navbar-toggler code"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse code" id="navbarNav">
+            <ul className="navbar-nav code">
+              <li className="nav-item code" key="About">
+                <a
+                  className="nav-link code-link"
+                  href="#about"
+                  style={styles.bright}
+                >
+                  About
+                </a>
+              </li>
+              <li className="nav-item code-link" key="Code">
+                <a className="nav-link code" href="#code" style={styles.bright}>
+                  Code
+                </a>
+              </li>
+              <li className="nav-item code-link" key="Information">
+                <a
+                  className="nav-link code"
+                  href="#information"
+                  style={styles.bright}
+                >
+                  Information
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
